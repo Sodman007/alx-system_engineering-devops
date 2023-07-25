b02303e83a74491b037638a522b5d900250892a0
Sodman007 <gsodiq123@gmail.com>
2023-07-25 10:50:20 -0700

    task on 0x0B. SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,26 +1,24 @@
-# Define the user and private key path
-$ssh_user = 'root'
-$private_key_path = '/home/root/.ssh/school'
-
-# Create the ~/.ssh directory if it doesn't exist
-file { '/home/root/.ssh':
-  ensure => directory,
-  mode   => '0700',
-  owner  => $ssh_user,
-  group  => $ssh_user,
+# Define a custom type for managing the SSH client configuration
+define ssh_config_entry(
+  $ensure = present,
+  $key,
+  $value,
+) {
+  file_line { "ssh_config_${title}":
+    path  => '/etc/ssh/ssh_config', # Adjust the path if your SSH client configuration file is located elsewhere.
+    line  => "${key} ${value}",
+    match => "^\\s*${key}\\s+.*",
+  }
 }
 
-# Create the SSH client configuration file
-file { '/home/root/.ssh/config':
-  ensure  => present,
-  content => "
-    Host 212815-web-01
-      HostName 54.152.59.63
-      User ubuntu
-      IdentityFile $private_key_path
-      PasswordAuthentication no
-  ",
-  mode    => '0600',
-  owner   => $ssh_user,
-  group   => $ssh_user,
+# Configure SSH to use the private key ~/.ssh/school
+ssh_config_entry { 'identity_file':
+  key   => 'IdentityFile',
+  value => '~/.ssh/school',
+}
+
+# Disable password authentication
+ssh_config_entry { 'password_authentication':
+  key   => 'PasswordAuthentication',
+  value => 'no',
 }
